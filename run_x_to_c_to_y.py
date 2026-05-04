366a5c40970a4955b4e615c3edc7e6c1ff61f8ec
Amin Hajjari <107117597+aminhajjari@users.noreply.github.com>
2026-05-04 22:03:40 +0330

    Update run_x_to_c_to_y.py

--- run_x_to_c_to_y.py
+++ run_x_to_c_to_y.py
@@ -530,7 +530,7 @@ if __name__ == "__main__":
         x_to_c(model_name=args.model, dataset=args.dataset,
                 concept_reference_dict=args.concept_reference_dict,
                 split=args.split, raw_values=args.raw_values,
-                predict_for_train_set=args.predict_for_train_set) 
+                predict_for_train_set=args.predict_for_train_set, data_path=args.data_path) 
             
     c_to_y(model_name=args.llm, dataset=args.dataset, ckpt=args.ckpt, split=args.split, raw_values=args.raw_values, concept_extractor=args.concept_extractor, report_path=args.report_path, use_demos=args.use_demos, n_demos=args.n_demos, ground_truth_concepts=args.gt_concepts)
     classification(model_name=args.llm, dataset=args.dataset, ckpt=args.ckpt, split=args.split, ground_truth_concepts=args.gt_concepts,  raw_values=args.raw_values, concept_extractor=args.concept_extractor, n_demos=args.n_demos)
